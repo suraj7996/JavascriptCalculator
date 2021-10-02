e7cb045f1f31743a61cc13eb326c848ffb4fceb5
suraj7996 <surajnepali7896@gmail.com>
2021-10-02 17:29:53 +0545

    Deleted and modified

--- calcJs.js
+++ calcJs.js
@@ -1,18 +1,25 @@
- 
- function clears()
+
+//To clear the input box
+
+ function clears() //clear() use garna napaine, maybe clear() is the function of JS
  {
     document.getElementById("valueIs").value =""
-    document.getElementById("ans").value =""
+    document.getElementById("ans").value =""  //Seperate box for answer is created.
  }
+
+
+//To display the numbers and expressions 
 
  function disp(val){
      document.getElementById("valueIs").value += val;
-     console.log(val);
  }
+
+
+ //To calculate the values
 
  function calculate(){
      let x = document.getElementById("valueIs").value;
-     let y = eval(x);
+     let y = eval(x); //eval() calculates the values according to the expressions provided.
 
-     document.getElementById("ans").value = y;
+     document.getElementById("ans").value = y;  //answer is displayed in different box
  }
